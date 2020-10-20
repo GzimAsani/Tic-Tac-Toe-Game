66253035fb2995c564d7c0055066069a6b7e0439
CornerstoneII <seunmyles@gmail.com>
2020-10-20 10:00:32 +0100

    Add display_board method to main file

--- bin/main.rb
+++ bin/main.rb
@@ -1,20 +1,18 @@
 # Display a welcome message.
 
-# Prompt the 2 players for their names
-# Assign pieces to the players like 'X' or 'O'. randomly.
-# first to play has 'X' second has 'O': Deciding who gets to start.
 #
 # The Game Loop.
 #
 # 1. Display the board.
-# 2. Assign Tokens 'X' or 'O' to each player.
-# 3. Determine which player turn it is
-# 4. Prompt player to select a valid number for a cell position (1 - 9).
-# 5. if valid, update the board and switch turns
-# 6. else prompt player to type-in a valid number.
-# 7. if draw, display a message that says selected move is a draw move
-# 8. if win, display a message that says selected move is a winning move
-# 9. Exit on win or draw or go back to  1.
+# 2. Prompt the 2 players for their names
+# 3. Prompt each player for token ('X' or 'O').
+# 4. Determine which player turn it is.
+# 5. Prompt player to select a valid number for a cell position (1 - 9).
+# 6. if valid, update the board and switch turns
+# 7. else prompt player to type-in a valid number.
+# 8. if draw, display a message that says selected move is a draw move
+# 9. if win, display a message that says selected move is a winning move
+# 10. Reset, Exit on win or draw or go back to  1.
 
 puts 'WELCOME TO THIS TIC-TAC-TOE GAME: created by: '
 puts "Press 'ENTER' to begin"
@@ -24,7 +22,15 @@ class TicTacToe
   attr_accessor :board
 
   def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @board = Array.new(9, ' ')
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts " ----------- "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts " ----------- "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 end
 # puts "\nPlayer 1:"
