e2f8f184d2407a7366e609a9c5c58e51b77bf1e4
CornerstoneII <seunmyles@gmail.com>
2020-10-20 09:35:47 +0100

    Define a class and add attr_accessor to main file

--- bin/main.rb
+++ bin/main.rb
@@ -20,28 +20,31 @@ puts 'WELCOME TO THIS TIC-TAC-TOE GAME: created by: '
 puts "Press 'ENTER' to begin"
 gets
 
-puts "\nPlayer 1:"
-Player1 = gets.chomp
-
-puts "\nPlayer 2:"
-Player2 = gets.chomp
-
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-
-def display_board(board)
-  %(#{board[0]} | #{board[1]} | #{board[2]}
-----------
-#{board[3]} | #{board[4]} | #{board[5]}
-----------
-#{board[6]} | #{board[7]} | #{board[8]}
-  )
+class TicTacToe
+  attr_accessor :board
 end
+# puts "\nPlayer 1:"
+# Player1 = gets.chomp
 
-game_piece = 'X'
+# puts "\nPlayer 2:"
+# Player2 = gets.chomp
 
-loop do
-  puts display_board(board)
-  square_number = gets.chomp.to_i
-  board[square_number - 1] = game_piece
-  game_piece = game_piece == 'X' ? 'O' : 'X'
-end
+# board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+# def display_board(board)
+#   %(#{board[0]} | #{board[1]} | #{board[2]}
+# ----------
+# #{board[3]} | #{board[4]} | #{board[5]}
+# ----------
+# #{board[6]} | #{board[7]} | #{board[8]}
+#   )
+# end
+
+# game_piece = 'X'
+
+# loop do
+#   puts display_board(board)
+#   square_number = gets.chomp.to_i
+#   board[square_number - 1] = game_piece
+#   game_piece = game_piece == 'X' ? 'O' : 'X'
+# end
