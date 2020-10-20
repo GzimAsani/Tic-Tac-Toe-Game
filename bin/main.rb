b8be7d8be7b1974c4e0302dcc232e33c4ccacd83
CornerstoneII <seunmyles@gmail.com>
2020-10-20 11:10:53 +0100

    Add select_player method to choose who starts game

--- bin/main.rb
+++ bin/main.rb
@@ -4,8 +4,8 @@
 # The Game Loop.
 #
 # 1. Display the board.
-# 2. Prompt each player for token ('X' or 'O').
-# 3. Determine which player turn it is.
+# 2. Prompt a player for token ('X' or 'O').
+# 3. Determine which player to start the game and turn it is.
 # 4. Prompt player to select a valid number for a cell position (1 - 9).
 # 5. if valid, update the board and switch turns
 # 6. else prompt player to type-in a valid number.
@@ -15,44 +15,29 @@
 
 puts 'WELCOME TO THIS TIC-TAC-TOE GAME: created by: '
 
-puts 'Player Enter your token of choice: X or O '
-Player1 = gets.chomp
-
 puts "Press 'ENTER' to begin"
 gets
 
 class TicTacToe
-  attr_accessor :board
+  attr_accessor :board, :players
 
   def initialize
     @board = Array.new(9, ' ')
+    @players = { Player1: 'X', Player2: 'O' }
   end
 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts " ----------- "
+    puts ' ----------- '
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts " ----------- "
+    puts ' ----------- '
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
+  def select_player
+    @players.sample
+  end
 end
-# puts "\nPlayer 1:"
-# Player1 = gets.chomp
-
-# puts "\nPlayer 2:"
-# Player2 = gets.chomp
-
-# board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-
-# def display_board(board)
-#   %(#{board[0]} | #{board[1]} | #{board[2]}
-# ----------
-# #{board[3]} | #{board[4]} | #{board[5]}
-# ----------
-# #{board[6]} | #{board[7]} | #{board[8]}
-#   )
-# end
-
 # game_piece = 'X'
 
 # loop do
