2da708c34eeb9860e9eb2be48682c5b0d0e141b1
Gzim <gzimasanni@gmail.com>
2020-10-22 10:52:15 +0200

    Added board and player classes

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+class Player
+    attr_reader :name, :symbol
+    attr_accessor :current
+
+
+    def initialize(name, symbol)
+        @name = name
+        @symbol = symbol
+        @current = nil
+    end
+
+    def who_is_first
+        
+    end
+end
